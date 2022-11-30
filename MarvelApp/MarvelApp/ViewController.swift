93cb009a822da1f75b9a1e396e92ac18d3b114cb
Chirag-Acquire <32319729+chirag84@users.noreply.github.com>
2022-11-30 21:29:35 +0530

    initial commit
    feat: pod setup

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Chirag on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
